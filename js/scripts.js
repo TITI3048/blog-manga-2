79b0ae8800b5279dd99b322f36d519fe0926625c
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-11-07 21:02:14 +0100

    mise en page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Blog Manga loaded!");
-    // Ajoute ici des fonctionnalités interactives, comme le filtre par catégorie
 });
 document.addEventListener("DOMContentLoaded", function() {
-    // Ajoute des commentaires dynamiques pour chaque manga
     const mangaCards = document.querySelectorAll(".manga-card");
 
     mangaCards.forEach(card => {
@@ -11,8 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
             alert(`Bienvenue dans la section ${card.querySelector("h3").innerText}!`);
         });
     });
-
-    // Fonctionnalité d'ajout de commentaires
+    
     const commentButton = document.querySelector("button");
     commentButton.addEventListener("click", function() {
         const commentBox = document.querySelector("textarea");
